93c4371b03545e986482c474579a256f35d38fd5
Leo <leo881003@gmail.com>
2022-04-22 03:42:18 +0800

    Complete part2

--- Project/BRG1-network-init.sh
+++ Project/BRG1-network-init.sh
@@ -17,9 +17,23 @@ ip link add gretap1 type gretap \
 ip link set gretap1 up
 ip fou add port 50056 ipproto 47
 
+# Start ovs server
+ovsdb-server --detach --remote=punix:/var/run/openvswitch/db.sock
+ovs-vswitchd --detach
+
 # Add links to bridge
-ip link add br0 type bridge
+ovs-vsctl add-br br0
+ovs-vsctl set bridge br0 protocols=OpenFlow10,OpenFlow11,OpenFlow12,OpenFlow13,OpenFlow14,
 ip link set br0 up
-ip link set eth0 master br0
-ip link set gretap1 master br0
+ovs-vsctl add-port br0 eth0
+ovs-vsctl add-port br0 gretap1
+ovs-vsctl set-fail-mode br0 standalone
+ovs-ofctl -O OpenFlow14 add-meter br0 meter=1,kbps,band=type=drop,rate=1000
+ovs-ofctl -O OpenFlow14 add-flow br0 actions=meter:1,normal
+
+## Original Linux bridge
+#ip link add br0 type bridge
+#ip link set br0 up
+#ip link set eth0 master br0
+#ip link set gretap1 master br0
 
